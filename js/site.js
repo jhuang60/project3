0897d4d9c60d186e1e501df006ad09d825936614
Jiayu Huang <jhuang60@hawk.iit.edu>
2017-12-05 23:01:14 -0600

    sound effect added when click submit

--- js/site.js
+++ js/site.js
@@ -1,11 +1,24 @@
 $.noConflict();
 
 (function($){
+
+  $("#popsound").remove();
+  $(document).ready(function(){
+    function messagesubmit() {
+      var popsound = new Audio('media/pop.mp3');
+      popsound.play();
+    }
+   $("#submitForm").on("click" , function(){
+   messagesubmit();
+    }
+    );
+  });
+
   $('#questionform').on('submit', function(e){
   e.preventDefault();
   var name = $('#nameinput').val();
-  var email = $('emailinput').val();
-  var question = $('questioninput').val();
-  console.log("form submitted", name);
+  var email = $('#emailinput').val();
+  var question = $('#questioninput').val();
+  console.log("form submitted", name, email, question);
   });
 })(jQuery);
