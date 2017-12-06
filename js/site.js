332a37d437fb20eee081442ae5f7cd42e16d74ef
Jiayu Huang <jhuang60@hawk.iit.edu>
2017-12-06 00:06:02 -0600

    if statement to alert user if theinput was incorrect

--- js/site.js
+++ js/site.js
@@ -3,22 +3,35 @@ $.noConflict();
 (function($){
 
   $("#popsound").remove();
+
   $(document).ready(function(){
     function messagesubmit() {
       var popsound = new Audio('media/pop.mp3');
       popsound.play();
     }
+
    $("#submitForm").on("click" , function(){
    messagesubmit();
-    }
-    );
+    });
+
   });
 
   $('#questionform').on('submit', function(e){
+
   e.preventDefault();
+
   var name = $('#nameinput').val();
   var email = $('#emailinput').val();
   var question = $('#questioninput').val();
+
+  if (name == '' || email == '' || question == '') {
+           alert("Please fill in all the fields");
+           return false;
+       }
+
   console.log("form submitted", name, email, question);
+  producePrompt ("Correct");
+
   });
+
 })(jQuery);
